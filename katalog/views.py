d8f294f19195b3f3be7202b98790d4b0c1386cd3
Farras Hafizhudin <103520002+farrasjay@users.noreply.github.com>
2022-09-14 00:04:58 +0700

    implement list_katalog

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': data_barang_katalog,
+        'nama': 'Farras Hafizhudin Indra Wijaya',
+        'npm' : '2106652682'
+    }
+    return render(request, "katalog.html", context)
