cb0ef75911f1291ed23f377e419b70d5dffe7560
Farras Hafizhudin <103520002+farrasjay@users.noreply.github.com>
2022-09-14 00:20:41 +0700

    add subcatalog items

--- katalog/urls.py
+++ katalog/urls.py
@@ -5,5 +5,5 @@ from katalog.views import show_katalog
 app_name = 'katalog'
 
 urlpatterns = [
-    path('', show_katalog, name='show_katalog'),
+    path('items', show_katalog, name='show_katalog'),
 ]
